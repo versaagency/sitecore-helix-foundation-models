cbc9357528151d17561644d5fa2194bda296caec
Liam Coghill <liam.coghill@deepend.melbourne>
2018-09-20 15:14:23 +1000

    Add image extension method

---
+++ code/Extensions/ImageExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Glass.Mapper.Sc;
+using Glass.Mapper.Sc.Fields;
+using Sitecore.Data.Items;
+
+namespace Sitecore.Foundation.Models.Extensions
+{
+    public static class ImageExtensions
+    {
+        public static Item Item(this Image image)
+        {
+            if (image == null)
+            {
+                throw new ArgumentNullException(nameof(image));
+            }
+
+            var context = new SitecoreContext();
+            return context.GetItem<Item>(image.MediaId);
+        }
+    }
+}
